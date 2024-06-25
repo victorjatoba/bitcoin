1fe6771b850b6688f363c24f46d1e409b6920f7e
victorjatoba <vmgjatoba@gmail.com>
2024-06-25 19:06:51 +0100

    add first version of byzantine generals problem code in go

--- byzantine-generals-problem/src/main.go
+++ byzantine-generals-problem/src/main.go
@@ -3,5 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hi")
+	var G = [6]string{"Alexander the Great", "Julius Caesar", "Napoleon Bonaparte", "Genghis Kan", "George Washington", "Duke of Caxias"}
+	g := len(G)
+	var M = []string{}
+	var B = []string{}
+	var b int
+
+	for i := 0; i < g; i++ {
+		M = append(M, ReadMessage(G[i]))
+	}
+
+	MajorityDecision := CheckMajority(G, M)
+	if (MajorityDecision) {
+		fmt.Println("A supermajority of Generals agree to Attack")
+	}
+
+	B = CheckBesieged(G, M, MajorityDecision)
+	b = len(B)
+
+	fmt.Println("Names of besieged generals:")
+	for i := 0; i < b; i++ {
+		fmt.Println(B[i])
+	}
+}
+
+func ReadMessage(general string) string {
+	return "Message from " + general
+}
+
+func CheckMajority(G [6]string, M []string) bool {
+	return true
+}
+
+func CheckBesieged(G [6]string, M []string, MajorityDecision bool) []string {
+	slicedG := G[:]
+	return slicedG
 }
